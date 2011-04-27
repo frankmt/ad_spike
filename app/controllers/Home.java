b59d19834a864402cb4d968f1a790ee55e0c182c
Francisco Trindade <frank.trindade@gmail.com>
2011-04-27 15:40:09 +0530

    removing unused code

--- app/controllers/Home.java
+++ app/controllers/Home.java
@@ -11,11 +11,6 @@ public class Home extends Controller {
 
     private static Random generator = new Random(19580427);
 
-    @Before
-    static void createGenerator() {
-        // generator = new Random(19580427);
-    }
-
     public static void index() {      
         int randomIndex = generator.nextInt(3);
         String imageName = "image" + randomIndex;
